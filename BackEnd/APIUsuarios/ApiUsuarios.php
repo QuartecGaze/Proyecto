1f75d0bd781dc87c42c0ae5dd23ff7b90430a58e
Diego alonso <diegoalonsorios4@gmail.com>
2025-07-06 00:09:41 -0300

    Habilitar CORS PHP

--- BackEnd/APIUsuarios/ApiUsuarios.php
+++ BackEnd/APIUsuarios/ApiUsuarios.php
@@ -6,6 +6,10 @@
     require_once __DIR__ .'/Modelos/Admin.php';
     require_once __DIR__ .'/Modelos/Interesado.php';
     require_once __DIR__ .'/BDConeccion.php';
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(200);
