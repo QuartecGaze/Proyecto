c6b09dd9962bcf3d2a4a77c1199b9344a9cdb375
AlainArce <alainarce39@gmail.com>
2025-11-19 12:41:19 -0300

    Modelo Funcional Usuario
    Se subio el modelo funcional de usuario el cual muestra datos ficticios para la muestra del programa.

--- BackEnd/BDConeccion.php
+++ BackEnd/BDConeccion.php
@@ -1,7 +1,7 @@
 <?php
-	$hostname = '192.168.1.50';//localhost
+	$hostname = 'localhost';//localhost
 	$username = 'root';//root
-	$password = 'Root';//root
+	$password = '';//root
 	$database = 'cooperativa';//cooperativa
 	$conn = mysqli_connect($hostname, $username, $password, $database);
 	if(!$conn){
